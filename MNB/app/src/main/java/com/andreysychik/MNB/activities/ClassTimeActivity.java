81d32416564ac2a2b028d1864c8b0dc4bcb4e727
AndreySychik <andrey.sy4ik@mail.ru>
2016-12-23 09:22:46 +0300

    Change layout

---
+++ MNB/app/src/main/java/com/andreysychik/MNB/activities/ClassTimeActivity.java
@@ -0,0 +1,116 @@
+package com.andreysychik.MNB.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.andreysychik.MNB.R;
+import com.andreysychik.MNB.entity.ClassTime;
+import com.andreysychik.MNB.filework.ClassTimeFile;
+
+import java.util.ArrayList;
+
+public class ClassTimeActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_time);
+        setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
+
+        /*
+        // next code creates '='-like icon(with 3 horizontal sticks) in the upper left corner
+        // to open left side navigation bar/
+        // Do we need this? actually not
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+        this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        */
+
+        ((NavigationView) findViewById(R.id.nav_view)).setNavigationItemSelectedListener(this);
+        fillContent();
+    }
+
+    private void fillContent() {
+        ArrayList<ClassTime> data = ClassTimeFile.getData();
+
+    }
+
+    private void messageEmpty() {
+        LinearLayout content = (LinearLayout) findViewById(R.id.content_class_time);
+        TextView messageEmptyText = new TextView(this);
+        messageEmptyText.setText(R.string.class_time_list_is_empty_message);
+        ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        content.addView(messageEmptyText, lp);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.edit_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_edit) {
+            startActivity(new Intent(this, EditClassTimeActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        // TODO replace with switch
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
